59c3fc79d6731d68749450e2e6fde7d1abf3ce10
SArrieta02 <stevenbalestero02@gmail.com>
2026-04-17 11:01:05 -0600

    Modificaciones

--- app.js
+++ app.js
@@ -55,6 +55,42 @@ app.post('/api/books', (req, res) => {
      res.status(201).json(newBook);
 });
 
+app.put('/api/books/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const{ title, author } = req.body;
+
+    const book = books.find(b => b.id === id);
+
+    if (!book) {
+        return res.status(404).json({
+            message: 'No encontrado'
+        });
+    }
+
+    book.title = title;
+    book.author = author;
+
+    res.json(book); 
+
+})
+
+app.delete('/api/books/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const index = books.findIndex(b => b.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({
+            message: 'No encontrado'
+        });
+    }
+
+    books.splice(index, 1);
+
+    res.json({
+        message: 'Eliminado'
+    });
+});
 app.get('/', (req, res) =>{
     res.send('Hola todos');
 })
